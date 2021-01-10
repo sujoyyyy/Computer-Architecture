c9ebc682c630f61fe02abc5813de2f85667eb607
Sujoy Datta <sujoydatta.2009@gmail.com>
2021-01-10 18:05:54 +0530

    changes

--- Floating Point Adder(Single Precision)/FPA.v
+++ Floating Point Adder(Single Precision)/FPA.v
@@ -8,12 +8,11 @@ module top;
 
     reg [31:0] I1,I2;
     wire [31:0]A,B;
-    Swap SW(I1,I2,A,B);
-
+    Swap SW(I1,I2,A,B); //First step check whether A > B and swap accordingly
 
     wire S1,S2;
-    wire [7:0] E1,E2;
-    wire [22:0] M1,M2;
+    wire [7:0] E1,E2; //exponents
+    wire [22:0] M1,M2; //mantissas
 
     Split SP1(A,S1,E1,M1);
     Split SP2(B,S2,E2,M2);
@@ -22,11 +21,10 @@ module top;
     assign Ediff = E1 - E2;
 
     wire [31:0] N1,N2,N3;
-    assign N1 = {|E1,M1};   //Reduction or handles zeroes
-    assign N2 = {|E2,M2};   //and denormal numbers
+    assign N1 = {|E1,M1};   
+    assign N2 = {|E2,M2};   
 
     BarrelShift BS(N2,Ediff,N3);
-
     wire [31:0]N4;
     assign N4 = {32{S1^S2}}^N3;
 
@@ -39,6 +37,7 @@ module top;
 
     integer i =0;
 
+    //normalising the final result
     always @(Sum)
     begin
         if(Sum[24]==1)
@@ -67,16 +66,18 @@ module top;
     reg [31:0]out;
     always @ (E3 or M3)
     begin
-        out = {S1,{8{|Sum}} & E3,M3};
+        out = {S1,{8{|Sum}} & E3,M3}; //combining the final answers to ieee-754 rep
     end
 
+    //Testbench
     initial
     begin
-        #0 I1={1'b0,{7'b1000001},24'b010000000000000000000000}; I2={1'b0,{7'b1000000},24'b110000000000000000000000};
+        #0 I1={1'b0,{8'b10000001},23'b10000000000000000000000}; I2={1'b0,{8'b10000010},23'b00000000000000000000000};
+        #10 I1={1'b0,{8'b10000010},23'b00111000000000000000000}; I2={1'b0,{8'b01111110},23'b00100000000000000000000};
     end
     initial
     begin
-        $monitor($time, " A=%b B=%b\tC=%b\t\n",I1,I2,out);
+        $monitor($time," A=%b B=%b\tC=%b\t\n",I1,I2,out);
         $dumpfile("fpa.vcd");
 		$dumpvars;
     end
